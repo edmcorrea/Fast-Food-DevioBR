681a24e16fbe9bc6d2e58a03154429dfd8bf6417
edcorrea <edm.correa@hotmail.com>
2023-10-27 17:59:43 -0300

    feat: Frontend - Folders Organization

---
+++ frontend/src/app/components/Categories/index.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import reactLogo from '../../../assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import 't Orders.css'
+
+function Categories() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Categories
