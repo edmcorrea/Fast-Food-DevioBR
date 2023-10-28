29dc5cce30d6a2515d121b134bfe383a5f30165a
edcorrea <edm.correa@hotmail.com>
2023-10-27 22:56:16 -0300

    refactor: Frontend - Updated mocks on categories mock and products mock

---
+++ frontend/src/app/services/categories.mock.jsx
@@ -0,0 +1,18 @@
+export const categoriesMock = [
+  {
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Combos"
+  },
+  {
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Acompanhamentos"
+  },
+  {
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Bebidas"
+  },
+  {
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Sobremesas"
+  },
+];
---
+++ frontend/src/app/services/products.mock.jsx
@@ -0,0 +1,37 @@
+export const productsMock = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Smash da Casa",
+    price: 30.50,
+    ingredientes: ["2x hamburgues 180g", "alface", "picles"]
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Smash da Casa",
+    price: 30.50,
+    ingredientes: ["2x hamburgues 180g", "alface", "picles"]
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Smash da Casa",
+    price: 30.50,
+    ingredientes: ["2x hamburgues 180g", "alface", "picles"]
+  },
+  {
+    id: 4,
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Smash da Casa",
+    price: 30.50,
+    ingredientes: ["2x hamburgues 180g", "alface", "picles"]
+  },
+  {
+    id: 5,
+    img: "https://i.pinimg.com/originals/f1/f9/8e/f1f98e74906d2c48ff4ec8a40e792d65.png",
+    name: "Smash da Casa",
+    price: 30.50,
+    ingredientes: ["2x hamburgues 180g", "alface", "picles"]
+  }
+];
