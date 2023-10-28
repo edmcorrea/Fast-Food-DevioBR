eb25446d50ca2f52ea507f801065602905ada510
edcorrea <edm.correa@hotmail.com>
2023-10-27 23:10:51 -0300

    feat: frontend - insert remove on order summary function

--- frontend/src/app/components/OrderSummary/index.jsx
+++ frontend/src/app/components/OrderSummary/index.jsx
@@ -3,7 +3,12 @@ import { useContext } from "react";
 import Context from "../../context/Context";
 
 function OrderSummary() {
-  const { summaryList } = useContext(Context);
+  const { summaryList, setSummaryList } = useContext(Context);
+
+  const removeOnSummaryList = (id) => {
+    const filteredWithOutProduct = summaryList.filter((product) => product.id != id);
+    setSummaryList(filteredWithOutProduct);
+  };
 
   return (
     <>
@@ -12,7 +17,7 @@ function OrderSummary() {
         <div>
           <h1>Resumo dos pedidos</h1>
           {summaryList.map((product) => (
-            <div key={product.name}>
+            <div key={product.id}>
               <img src={product.img}
                 className="img"
                 alt={product.name}
@@ -23,8 +28,10 @@ function OrderSummary() {
                   <p>{ingrediente}</p>
                 </div>
               ))}
-              <p>{`R$${product.price}`}</p>
-              <button>
+              <p>{`R$ ${product.price.toFixed(2)}`}</p>
+              <button
+                onClick={()=> removeOnSummaryList(product.id)}
+              >
                 Remover
               </button>
             </div>
