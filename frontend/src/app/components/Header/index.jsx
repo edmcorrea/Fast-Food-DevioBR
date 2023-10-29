7e2e9f2a23c83160bdf57590c13246231813e4c8
edcorrea <edm.correa@hotmail.com>
2023-10-29 03:24:00 -0300

    feat: frontend - styles on header

--- frontend/src/app/components/Header/index.jsx
+++ frontend/src/app/components/Header/index.jsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
-// import 't Orders.css'
+import './Header.scss';
+import burguers from "../../../assets/burguers.png"
 
 function Header() {
 
   return (
     <>
-      <div>
+      <div className='header'>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src="https://png.pngtree.com/png-vector/20230131/ourmid/pngtree-fast-food-logo-design-use-to-your-business-png-image_6578591.png" className="logo react" alt="React logo" />
+          <img src={burguers} className="logo react" alt="React logo" />
         </a>
-        <Link to="/">Pedidos</Link>
-        <Link to="/kitchen">Cozinha</Link>
-        <Link to="/status">Retirada</Link>
+        <div className='header-links'>
+          <Link to="/">Pedidos</Link>
+          <Link to="/kitchen">Cozinha</Link>
+          <Link to="/status">Retirada</Link>
+        </div>
 
       </div>
     </>
