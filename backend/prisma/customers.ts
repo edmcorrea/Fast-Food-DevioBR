e6b63caa01e9a49b89a02625e6a4d937ff95acd4
edcorrea <edm.correa@hotmail.com>
2023-10-30 10:31:27 -0300

    refactor: backend - refactoring prisma config

---
+++ backend/prisma/customers.ts
@@ -0,0 +1,54 @@
+const customers = [
+  {
+    customerName: "Ricardo",
+    products: [{ name:"Hamburguer Novo", observation: "", quantity: 1}, { name:"Smash da Casa", observation: "", quantity: 1}],
+    status: "Preparing"
+  },
+  {
+    customerName: "Luiza",
+    products: [{ name:"Smash da Casa", observation: "", quantity: 2}],
+    status: "Preparing"
+  },
+  {
+    customerName: "Fernando",
+    products: [{ name:"Smash da Casa", observation: "", quantity: 1}],
+    status: "Preparing"
+  },
+  {
+    customerName: "Bruna",
+    products: [{ name:"Hamburguer Novo", observation: "", quantity: 1}],
+    status: "Completed"
+  },
+  {
+    customerName: "Jussara",
+    products: [{ name:"Smash da Casa", observation: "molho branco", quantity: 1}],
+    status: "Completed"
+  },
+  {
+    customerName: "Lucas",
+    products: [{ name:"Hamburguer Novo", observation: "", quantity: 1}, { name:"Smash da Casa", observation: "", quantity: 1}],
+    status: "Preparing"
+  },
+  {
+    customerName: "Gabriel",
+    products: [{ name:"Smash da Casa", observation: "", quantity: 2}],
+    status: "Preparing"
+  },
+  {
+    customerName: "Gouveia",
+    products: [{ name:"Smash da Casa", observation: "", quantity: 1}],
+    status: "Preparing"
+  },
+  {
+    customerName: "Sara",
+    products: [{ name:"Hamburguer Novo", observation: "", quantity: 4}],
+    status: "Completed"
+  },
+  {
+    customerName: "Vanessa",
+    products: [{ name:"Smash da Casa", observation: "molho branco", quantity: 1}],
+    status: "Completed"
+  },
+];
+
+export default customers;
--- backend/prisma/migrations/20231029192009_customer/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Customer" (
-    "codCustomer" SERIAL NOT NULL,
-    "customerName" TEXT NOT NULL,
-    "productIds" JSONB,
-    "status" TEXT NOT NULL,
-
-    CONSTRAINT "Customer_pkey" PRIMARY KEY ("codCustomer")
-);
