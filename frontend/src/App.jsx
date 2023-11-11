abce33ec8bdcaa08eb9a208b490cdb612937e656
edcorrea <edm.correa@hotmail.com>
2023-11-11 17:16:43 -0300

    fix: bug on App.jsx page - order page fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
-import Orders from './app/pages/Orders';
+import Order from './app/pages/Order';
 import Kitchen from './app/pages/Kitchen'
 import Status from './app/pages/Status'
 import OrderDetails from './app/pages/OrderDetails';
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <Routes>
-      <Route exact path="/" element={ <Orders /> } />
+      <Route exact path="/" element={ <Order /> } />
       <Route exact path="/order/details" element={ <OrderDetails /> } />
       <Route exact path="/kitchen" element={ <Kitchen /> } />
       <Route exact path="/status" element={ <Status /> } />
